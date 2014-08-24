766d9635edb1ebf7e0266dd742a0aaf1e2a4416c
Chen Zihui <hello@chenzihui.com>
2014-08-24 14:57:10 +0800

    Allow creating a new list item

--- src/list_item.js
+++ src/list_item.js
@@ -1,1 +1,11 @@
 'use strict';
+
+var Base = require('./base'),
+
+    ListItem;
+
+ListItem = Base.Model.extend({
+  tableName: 'list_items'
+},{});
+
+module.exports = ListItem;
--- test/models/list_item_spec.js
+++ test/models/list_item_spec.js
@@ -6,7 +6,37 @@ var List     = require('../../src/list'),
     db       = require('../support/db');
 
 describe('List item', function() {
-  it('should allow creating a new list item');
+
+  var listArgs = { name: 'Test List' },
+      _list;
+
+  beforeEach(function(done) {
+    List.createNew(listArgs)
+    .then(function(list) {
+      _list = list;
+      done();
+    });
+  });
+
+  afterEach(function(done) {
+    db.cleanUp().then(function() {
+      done();
+    });
+  });
+
+  it('should allow creating a new list item', function(done) {
+    var args = { list_id: _list.id, title: 'New Item' },
+        listItem;
+
+    ListItem.createNew(args)
+    .then(function(result) {
+      listItem = result.toJSON();
+
+      expect(listItem.title).to.equal(args.title);
+      expect(listItem.list_id).to.equal(_list.id);
+      done();
+    });
+  });
 
   it('should allow retrieving a single item');
   it('should allow retrieving items from a particular list');
